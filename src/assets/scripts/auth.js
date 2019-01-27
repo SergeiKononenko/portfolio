d4e6abfc6a4bb3f90fff7f6586aa36f71e50577f
ds <knnnk.srg@gmail.com>
2019-01-28 00:38:35 +0300

    прелоадер правки

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,4 +1,7 @@
-
+import preloader from "./modules/preloader";
+document.addEventListener("DOMContentLoaded", e => {
+    preloader().loaded();
+   });
 
 import "./modules/flip";
 import "./modules/parallax";
